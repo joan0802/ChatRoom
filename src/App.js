eb7ec30ae98ef6cb8532d9849d1c75ca72439881
111062123 <joan040802@gmail.com>
2024-04-25 00:55:23 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App bg-main'>
+            <div className="bg-main flex min-h-screen items-center justify-center">
+                <div className='w-2/5 h-full bg-white rounded-lg bg-card p-10 gap-6'>
+                    <div className='gap-3'>
+                        <div className='info-format'>User Name: </div>
+                        <input type="text" className='info-input' placeholder="User Name" />
+                    </div>
+                    <div>
+                        <div className='info-format'>Email Address: </div>
+                        <input type="text" className='info-input' placeholder="Email Address" />
+                    </div>
+                    <div>
+                        <div className='info-format'>Password: </div>
+                        <input type="password" className='info-input' placeholder="Password" />
+                    </div>
+                    <button className='bg-main rounded-lg py-1 px-2 mt-2'>Login</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
