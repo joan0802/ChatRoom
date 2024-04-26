b1e782be59d06a2f5c328bc44c859d8ae16aa801
111062123 <joan040802@gmail.com>
2024-04-26 19:50:33 +0800

    add login, sign up, google login functions

--- src/Components/LoginSignUp/LoginSignUp.jsx
+++ src/Components/LoginSignUp/LoginSignUp.jsx
@@ -2,9 +2,57 @@ import React from "react";
 import { useState } from "react";
 import icon from '../../img/favicon.png';
 import './LoginSignUp.css';
+import { auth } from '../../firebase/firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 export default function LoginSignUp() {
     const [userAction, setUserAction] = useState('Login');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [userName, setUserName] = useState('');
+
+    async function SignUp () {
+        if(userAction === 'Sign Up') {
+            await createUserWithEmailAndPassword(auth, email, password)
+            .then((user) => {
+                alert('Sign up successfully!');
+            })
+            .catch((error) => {
+                alert(error.message);
+            });
+        }
+        else
+            setUserAction('Sign Up');
+        setEmail('');
+        setPassword('');
+        setUserName('');
+    }
+    async function Login() {
+        if(userAction === 'Login') {
+            await signInWithEmailAndPassword(auth, email, password)
+            .then((user) => {
+                alert('Login successfully!');
+            })
+            .catch((error) => {
+                alert(error.message); 
+            });
+        }
+        else
+            setUserAction('Login');
+        setEmail('');
+        setPassword('');
+        setUserName('');
+    }
+    async function LoginWithGoogle() {
+        const provider = new GoogleAuthProvider();
+        await signInWithPopup(auth, provider)
+        .then((user) => {
+            alert('Login with Google successfully!');
+        })
+        .catch((error) => {
+            alert(error.message);
+        });
+    }
 
     return (
         <div className='App bg-main'>
@@ -16,25 +64,25 @@ export default function LoginSignUp() {
                     <div className='flex justify-center font-title'>{userAction}</div>
                     {userAction === 'Sign Up' && <div className='gap-3'>
                         <div className='info-format'>User Name: </div>
-                        <input type="text" className='info-input' placeholder="User Name" />
+                        <input type="text" className='info-input' value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="User Name" />
                     </div>}
                     <div>
                         <div className='info-format'>Email Address: </div>
-                        <input type="text" className='info-input' placeholder="Email Address" />
+                        <input type="text" className='info-input' value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
                     </div>
                     <div>
                         <div className='info-format'>Password: </div>
-                        <input type="password" className='info-input' placeholder="Password" />
+                        <input type="password" className='info-input' value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
                     </div>
                     <div className='flex gap-6 pt-2'>
                         <button 
-                            className={"rounded-lg py-1 px-2 mt-2 " + (userAction==='Login'? "font-button-highlight" : "font-button")}
-                            onClick={() => setUserAction('Login')} >
+                            className={"rounded-lg py-1 px-3 mt-2 " + (userAction==='Login'? "font-button-highlight" : "font-button")}
+                            onClick={() => Login()} >
                                 Login
                         </button>
-                        <button className='rounded-lg py-1 px-2 mt-2 font-button'>Login with Google</button>
-                        <button className={"rounded-lg py-1 px-2 mt-2 " + (userAction==="Sign Up" ? "font-button-highlight" : "font-button")}
-                            onClick={() => setUserAction('Sign Up')} >
+                        <button className='rounded-lg py-1 px-3 mt-2 font-button' onClick={() => LoginWithGoogle()}>Login with Google</button>
+                        <button className={"rounded-lg py-1 px-3 mt-2 " + (userAction==="Sign Up" ? "font-button-highlight" : "font-button")}
+                            onClick={() => SignUp()} >
                             Sign Up
                         </button>
                     </div>
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAqZB-dzZekRx_325RRyZf8YF_qMiu_o2U",
@@ -14,5 +15,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
